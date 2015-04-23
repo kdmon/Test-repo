643cdc1a14e758d06ddbe1f9a32fb21513699df2
kdmon <kdmon@users.noreply.github.com>
2015-04-23 16:39:28 +0100

    add testcomponent

---
+++ testcomponent.js
@@ -0,0 +1,12 @@
+define(["knockout", "text!./testcomponent.html"],function(ko, loadedTemplate){
+
+  function testcomponent() {
+    alert ("constructing test component");
+  }
+
+  return ({
+    viewModel: testcomponent;
+    template: loadedTemplate;
+  });
+  
+});
