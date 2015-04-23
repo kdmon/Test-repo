24f2bb4e22dae8da08ce5c6d82247a28a50834c3
kdmon <kdmon@users.noreply.github.com>
2015-04-23 16:04:38 +0100

    fix module loading

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var require = {
+
+require.config({
   shim: {
     "bootstrap": {
       "deps": ['jquery']
@@ -7,7 +8,14 @@ var require = {
   paths: {
     "jquery": "//code.jquery.com/jquery-2.1.1.min",
     "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text.min",
-    "knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min.js",
+    "knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min",
     "bootstrap": "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min"
   }
-};
+});
+
+
+define(["knockout", "text","jquery", "bootstrap"], function (ko,txt,$,bootstrap) {
+
+  alert("Loaded all deps?");
+  
+});
