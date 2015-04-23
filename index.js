8c7205a318ba79294793f0627b503309b9b21702
kdmon <kdmon@users.noreply.github.com>
2015-04-23 15:47:35 +0100

    add more modules

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
 var require = {
-    shim : {
-        "bootstrap" : { "deps" :['jquery'] }
-    },
-    paths: {
-        "jquery" : "//code.jquery.com/jquery-2.1.1.min",
-        "bootstrap" :  "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min"  
+  shim: {
+    "bootstrap": {
+      "deps": ['jquery']
     }
+  },
+  paths: {
+    "jquery": "//code.jquery.com/jquery-2.1.1.min",
+    "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text.min",
+    "knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min.js",
+    "bootstrap": "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min"
+  }
 };
