090c19a7ecdbc359749a1094e0aa881bffbb71d1
kdmon <kdmon@users.noreply.github.com>
2015-05-14 22:16:18 +0100

    Update file.

--- aedit.js
+++ aedit.js
@@ -4,101 +4,20 @@ $('#layout').w2layout({
     name: 'layout',
     panels: [
         { type: 'top', size: 50, resizable: false, style: pstyle, content: 'top'},
-        { type: 'left', size: 150, resizable: true, style: pstyle, content: 'split' },
+        { type: 'left', size: 100, resizable: true, style: pstyle, content: 'split' },
         { type: 'main', style: pstyle, content: 'split',
           title: 'Panel <span id="layout-preview-split" class="panel-button split-panel"></span>' +
           '<span id="layout-main-close" class="panel-button close-panel"></span>',
           tabs: {
-            name: "tabs",
-            active: 'taba',
-            tabs: [{
-              id: 'taba',
-              caption: 'Files',
-              closable: 'false'
-            }, {
-              id: 'tabb',
-              caption: 'Options',
-              closable: 'false'
-            }, {
-              id: 'tabc',
-              caption: 'Custom tab',
-              closable: 'true'
-            }],
             onClose: function(event) {
-              this.owner.click ('tab2');
+              //this.owner.click ('tab2');
             },
             onClick: function(event) {
               //w2ui.layout.html('main', 'Active tab: '+ event.target);
               this.owner.content('main', 'event' + event.target);
             }
           },
-          toolbar: {
-            items: [{
-              type: 'button',
-              id: 'save',
-              caption: 'Save',
-              icon: 'fa fa-save',
-              hint: 'Save file'
-            },{
-              type: 'break',
-              id: 'break1'
-            }, {
-              type: 'button',
-              id: 'undo',
-              caption: 'Undo',
-              icon: 'fa fa-reply',
-              hint: 'Undo last edit'
-            },{
-              type: 'button',
-              id: 'redo',
-              caption: 'Redo',
-              icon: 'fa fa-share',
-              hint: 'Redo last edit'
-            },{
-              type: 'break',
-              id: 'break3'
-            }, {
-              type: 'menu',
-              id: 'more',
-              caption: '',
-              icon: 'fa fa-bars',
-              arrow: false,
-              items: [{
-                text: 'Search',
-                icon: 'fa fa-search',
-              }, {
-                text: 'Replace',
-                value: 'Item Three',
-                icon: 'fa fa-edit'
-              }, {
-                text: 'Go to line',
-                icon: 'fa fa-level-down'
-              }, {
-                text: 'Clean up indentation',
-                value: 'Item Three',
-                icon: 'fa fa-magic'
-              },{
-                text: 'File history',
-                icon: 'fa fa-history'
-              }]
-            },{
-              type: 'spacer'
-            },{
-              type: 'button',
-              id: 'hide',
-              caption: '',
-              icon: 'fa fa-close'
-            },{
-              type: 'button',
-              id: 'split',
-              caption: '',
-              icon: 'fa fa-minus-square-o'
-            }],
-            onClick: function(event) {
-              if(event.target == "hide") w2ui['layout'].toggle('top', window.instant);
-              if(event.target == "split") w2ui['layout'].toggle('preview', window.instant);
-              console.log(event);
-            }}
+          toolbar: {},
           },
         { type: 'preview', size: '50%', resizable: true, hidden:true, style: pstyle, content: 'split',
           title: 'Sub-panel <span id="layout-preview-close" class="panel-button close-panel"></span>',
@@ -194,7 +113,7 @@ $('#layout').w2layout({
               console.log(event);
             }}
           },
-        { type: 'right', size: 200, resizable: true,style: pstyle, content: 'split' },
+        { type: 'right', size: 100, resizable: true,style: pstyle, content: 'split' },
         { type: 'bottom', size: 50, resizable: true, style: pstyle, content: 'bottom' }
     ]
 });
@@ -223,11 +142,91 @@ $().w2layout({
     ]
 });
 
+$().w2tabs({
+  name: 'maintabs',
+  active: 'taba',
+  tabs: [],
+});
+
+$().w2toolbar({
+  name: 'texttoolbar',
+  items: [{
+    type: 'button',
+    id: 'save',
+    caption: 'Save',
+    icon: 'fa fa-save',
+    hint: 'Save file'
+  },{
+    type: 'break',
+    id: 'break1'
+  }, {
+    type: 'button',
+    id: 'undo',
+    caption: 'Undo',
+    icon: 'fa fa-reply',
+    hint: 'Undo last edit'
+  },{
+    type: 'button',
+    id: 'redo',
+    caption: 'Redo',
+    icon: 'fa fa-share',
+    hint: 'Redo last edit'
+  },{
+    type: 'break',
+    id: 'break3'
+  }, {
+    type: 'menu',
+    id: 'more',
+    caption: '',
+    icon: 'fa fa-bars',
+    arrow: false,
+    items: [{
+      text: 'Search',
+      icon: 'fa fa-search',
+    }, {
+      text: 'Replace',
+      value: 'Item Three',
+      icon: 'fa fa-edit'
+    }, {
+      text: 'Go to line',
+      icon: 'fa fa-level-down'
+    }, {
+      text: 'Clean up indentation',
+      value: 'Item Three',
+      icon: 'fa fa-magic'
+    },{
+      text: 'File history',
+      icon: 'fa fa-history'
+    }]
+  },{
+    type: 'spacer'
+  },{
+    type: 'button',
+    id: 'hide',
+    caption: '',
+    icon: 'fa fa-close'
+  },{
+    type: 'button',
+    id: 'split',
+    caption: '',
+    icon: 'fa fa-minus-square-o'
+  }],
+  onClick: function(event) {
+    if(event.target == "hide") w2ui['layout'].toggle('top', window.instant);
+    if(event.target == "split") w2ui['layout'].toggle('preview', window.instant);
+    console.log(event);
+  }
+});
+
+setTimeout(function () {
+  
+//1. You can use w2ui object to find your tabs - w2ui[layout_name + '_' + panel + '_tabs'].add(...)
+//2. You can use panel to find your tabs - w2ui[layout_name].get(panel).tabs.add(...);
+//w2ui['layout'].get('main').tabs.add(w2ui['editortabs']);
 w2ui['layout'].content('left', w2ui['leftsplit']);
 w2ui['layout'].content('middle', w2ui['middlesplit']);
 w2ui['layout'].content('right', w2ui['rightsplit']);
 
-setTimeout(function () {
   $(".panel-button").on("click", function (event) {
     console.log(event);
     var id = event.target.id.split("-");
@@ -267,13 +266,14 @@ setTimeout(function () {
       });
       $(".w2ui-panel-tabs td").on("drop", function (event) {
         event.preventDefault();
-        console.log(event);
+        var target = event.currentTarget.id.split("_");
+        console.log(target);
         var id = event.originalEvent.dataTransfer.getData("text");
         var caption = $("#" + id).text();
-        var oldContainer = 'tabs';//$("#" + id).parent().attr(name);
-        var newContainer = 'tabs';//$("#" + event.currentTarget.id).parent().attr(name);
+        var oldContainer = target[1]; // parent
+        var newContainer = target[1];
         
-        w2ui[oldContainer].remove(id);
+        w2ui['tabs1'].remove(id);
         w2ui[newName].add({id: id, caption: caption});
         //$("#" + data).insertBefore($('#'+event.currentTarget.id));
         // Refresh tab widgets!
