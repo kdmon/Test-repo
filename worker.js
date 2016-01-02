1116d3b8361ced1589475d04d4c4a5bce51f50a6
kdmon <kdmon@users.noreply.github.com>
2016-01-02 12:44:39 +0100

    add json helper function

--- worker.js
+++ worker.js
@@ -1,3 +1,17 @@
+// json search helper fxn
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else if (i == key && obj[key] == val) {
+            objects.push(obj);
+        }
+    }
+    return objects;
+}
+
 onmessage = function(e) {
   
   console.log('Message received from main script');
