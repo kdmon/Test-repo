b1ddee82f1e53faa9ea880bf8864aa58b97ab3fc
kdmon <kdmon@users.noreply.github.com>
2016-02-05 12:26:02 +0000

    add w2ui

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-
 require.config({
-  shim: {
-    "bootstrap": {
-      "deps": ['jquery']
-    }
-  },
   paths: {
     "jquery": "//code.jquery.com/jquery-2.1.1.min",
+    "w2ui": "http://webappeditor.com/vitmalina/w2ui/w2ui-1.4/dist/w2ui.min",
     "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text.min",
-    "knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min",
-    "bootstrap": "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min"
+    "knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min"
+  },
+    shim: {
+    'w2ui': {
+      deps: ['jquery'], 
+      exports: 'w2ui'
+      }
   }
 });
 
 
-define(["knockout", "jquery"], function (ko,$) {
+define(["knockout", "jquery", "w2ui"], function (ko,$) {
 
   ko.components.register("testcomponent", {require: "testcomponent"});
   
