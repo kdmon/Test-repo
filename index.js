1950025a7fbd8ba625087aebcd6ef121472e87a1
kdmon <kdmon@users.noreply.github.com>
2016-02-05 23:16:37 +0000

    Update file.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require.config({
   paths: {
     "jquery": "//code.jquery.com/jquery-2.1.1.min",
-    "w2ui": "http://webappeditor.com/vitmalina/w2ui/w2ui-1.4/dist/w2ui.min",
+    "w2ui": "/vitmalina/w2ui/w2ui-1.4/dist/w2ui.min",
     "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text.min",
     "knockout": "//cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min"
   },
@@ -14,7 +14,7 @@ require.config({
 });
 
 
-define(["knockout", "jquery", "w2ui"], function (ko,$) {
+define(["knockout", "jquery", "w2ui", "w2uigithack", "w2uirawgit", "w2uirawgitcdn"], function (ko,$) {
   
   ko.components.register("testcomponent", {require: "testcomponent"});
   
