82a6d5a28ee2b75904ad0cf25200f82292c29592
kdmon <kdmon@users.noreply.github.com>
2016-05-15 00:37:12 +0100

    handle rename and delete file operations

--- aedit.js
+++ aedit.js
@@ -1,4 +1,4 @@
-// Globals ui ui ui 
+// Globals
 
 var toolbars = {};
 var buttons = {};
@@ -1941,20 +1941,118 @@ function fileTreeMenu (node) {
         startDoc(conf);
       }
     },
-    rename: { 
+    renameFile: { 
       label: "Rename",
       icon: "fa fa-edit",
       action: function () {
-        tree.edit(node);
+        
+        // Get old filename.
+        var old = node.text.replace(/\s+$/, ''); // trim right spaces
+        
+        tree.edit(node, null, function(node, success, cancelled) {
+          
+          // Check if filename was changed
+          if (!success || cancelled) return;
+          if (node.text.replace(/\s+$/, '')==old) return;
+          
+          // Check if file name is valid, otherwise revert to old filename.
+          // TODO: improve regex matching pattern.
+          if (node.text.indexOf('/') > -1) {
+            w2alert("Ignoring new filename due to invalid characters.");
+            tree.rename_node(node, old);
+            return;
+          }
+
+          // Checks passed, so now try to commit the name change.
+          
+          var repo = github.getRepo(node.data.user, node.data.repo);
+          var branch = node.data.branch;
+          var oldPath = node.data.path;
+          var parts = node.data.path.split('/');
+          parts.pop(); // remove old filename
+          var newPath = '';
+          
+          //prevent leading slash in new path!
+          if (parts.length>0) newPath = parts.join("/") + "/" + node.text;
+          else newPath = node.text;
+          
+          repo.moveFile(branch, oldPath, newPath, function(err) {
+            if (err) {
+              w2alert("File could not be renamed.");
+              console.log(err);
+              // revert to old file name
+              tree.rename_node(node, old);
+            }
+            else {
+              w2alert("File renamed successfully.");
+              //update node path here!
+              node.data.path = newPath;
+            }
+          });
+          
+        });
       }
     },
-    duplicate: { 
+    renameFolder: { 
+      label: "Rename",
+      icon: "fa fa-edit",
+      action: function () {
+        
+        // Get old folder name.
+        var old = node.text.replace(/\s+$/, ''); // trim right spaces
+        
+        tree.edit(node, null, function(node, success, cancelled) {
+          
+          // Check if folder name was changed
+          if (!success || cancelled) return;
+          if (node.text.replace(/\s+$/, '')==old) return;
+          
+          // Check if file name is valid, otherwise revert to old folder name.
+          // TODO: improve regex matching pattern.
+          if (node.text.indexOf('/') > -1) {
+            w2alert("Ignoring new folder name due to invalid characters.");
+            tree.rename_node(node, old);
+            return;
+          }
+
+          // Checks passed, so now try to commit the name change.
+          
+          var repo = github.getRepo(node.data.user, node.data.repo);
+          var branch = node.data.branch;
+          var oldPath = node.data.path;
+          var parts = node.data.path.split('/');
+          parts.pop(); // remove old folder name
+          var newPath = '';
+          
+          // Prevent leading slash in new path!
+          if (parts.length>0) newPath = parts.join("/") + "/" + node.text;
+          else newPath = node.text;
+          
+          repo.moveFolder(branch, oldPath, newPath, function(err) {
+            if (err) {
+              w2alert("Folder could not be renamed.");
+              console.log(err);
+              // revert to old folder name
+              tree.rename_node(node, old);
+            }
+            else {
+              w2alert("Folder renamed successfully.");
+              //update node path here!
+              node.data.path = newPath;
+            }
+          });
+          
+        });
+      }
+    },
+    /*duplicate: { 
       label: "Duplicate",
       icon: "fa fa-copy",
       action: function () {
         alert ("duplicate");
       }
     },
+    */
     remove: { 
       separator_after : true,
       label: "Delete",
@@ -1964,11 +2062,12 @@ function fileTreeMenu (node) {
         node.data.name + "</em>?", "Warning",
         function (result) {
           if (result === "Yes") {
+            var repo = github.getRepo(node.data.user, node.data.repo);
             var branch = node.data.branch;
             var path = node.data.path;
             repo.remove(branch, path, function(err) {
-              if(err) w2alert("File deleted");
-              else w2alert("File deleted successfully.");
+              if(err) {w2alert("File could not be deleted."); console.log(err)}
+              else { w2alert("File deleted successfully."); tree.delete_node(node);}
             });
           }
         });
@@ -2006,14 +2105,21 @@ function fileTreeMenu (node) {
       separator_after : true,
       icon: "fa fa-plus-square",
       action: function () {
-        
+        console.log("creating",node);
+        var newNode = tree.create_node(node);
+        tree.edit(newNode);
       }
     }
   };
 
-  if (node.data.type == "directory" || node.data.type == "binary") {
+  if (node.data.type == "directory") {
+    delete items.renameFile;
     delete items.edit;
     delete items.preview;
+  }
+
+  if (node.data.type == "file" || node.data.type == "binary") {
+    delete items.renameFolder;
   }
   
   return items;
@@ -2157,12 +2263,9 @@ function openProject (user, repository, branch, panelArea) {
       .on('move_node.jstree', function (e, data) {
         console.log("Moved node", data);
       })
-      .on('rename_node.jstree', function (e, data) {
-        console.log("Renamed node", data);
-        
+      .on('rename_node.jstree', function (e, node) {
         /*
         var old = node.text.replace(/\s+$/, ''); // trim right spaces
-
         instance.edit(node, null, function(node, success, cancelled) {
             if (!success || cancelled) return;
             if (node.text.replace(/\s+$/, '')==old) return;
