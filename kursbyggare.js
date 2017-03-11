7c4823d120050531d6fedeea160e608ecc7749c8
kdmon <kdmon@users.noreply.github.com>
2017-03-11 16:04:14 +0100

    Update file.

---
+++ kursbyggare.js
@@ -0,0 +1,74 @@
+var KB = {};
+
+KB.packages = [
+  {
+    title_sv : "Mobilapputvecklare med HTML5, CSS3 och JS",
+    title_en : "Mobile App Developer with HTML5, CSS3 and JS",
+    courses : ["html", "js", "app"]
+  },
+  {
+    title_sv : ""
+  },
+  {
+    
+  }
+];
+
+KB.courses = {
+  "term1-2017" : [
+    
+    // Inriktning 'Webbansvarig' - du uppdaterar innehåll, kommunicerar via 
+    // sociala media kanaler, osv.
+    
+    {
+      title_sv: "Webbdesign från grunden",
+      id : 'html'
+    },{
+      title_sv: "Webbpubliceringsverktyg",
+      id : 'cms'
+    },{
+      title_sv: "Kommunikation på webben och i sociala media",
+      id : 'comms'
+    },
+    
+    // bild-behandling
+    
+    
+    // UX
+    
+    {
+      title_sv: "Användarvänlighet och tillgänglighet på webben",
+      id : 'usability'
+    },{
+      title_sv: "Responsiva och mobilanpassade webbsidor med CSS3",
+      id : 'responsive'
+    },
+    // plus en valfri kurs 
+    
+    
+    // eCommerce
+    
+    {
+      title_sv: "eHandel, eMarknadsföring och SEO", id : 'ecom'
+    },
+    
+    
+    // Digital media
+    
+    {
+      title_sv: "Digital fotografering"
+    },{
+      title_sv: "Digital bildbehandling"
+    },{
+      title_sv: "Illustration och vektorgrafik"
+    },
+    
+    
+    {
+      title_sv: "Speldesign"
+    }
+  ]
+};
+
+
+
