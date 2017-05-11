ed5d5044cb8660577f5ed3e5f13e77168bed35db
kdmon <kdmon@users.noreply.github.com>
2017-05-11 09:57:13 +0200

    Update file.

--- kursbyggare.js
+++ kursbyggare.js
@@ -14,6 +14,11 @@ KB.packages = [
   }
 ];
 
+
+// Varje paket och inriktning består av två kurser plus en valfri kurs.
+// Man kan också välja att jobba på ett litet projekt under handledning
+
+
 KB.courses = {
   "term1-2017" : [
     
@@ -31,7 +36,8 @@ KB.courses = {
       id : 'comms'
     },
     
-    // bild-behandling
+    // optionals - recommended based on low no. of participants:
+    // photo, bild-behandling, mm 
     
     
     // UX
@@ -66,9 +72,104 @@ KB.courses = {
     
     {
       title_sv: "Speldesign"
+    },
+    
+    // Javascriptutvecklare
+    
+    {
+      title_sv: 'Webbprogrammering'
+    },
+    {
+      title_sv: 'NodeJS: Javascript på servern'
+    },
+    {
+      title_sv: 'Versionskontroll med Git och GitHub'
     }
   ]
 };
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+4 terminer med två cykler med 6 kurser var => 12 unika kurser, 24 kurser per år
+
+vår (cykel 1)
+
+sommar (cykel 2)
+
+höst (cykel 1)
+
+vinter (cykel 2)
+
+
+cykel 1
+
+1
+2
+
+3
+4
+
+5
+6
+
+cykel 2
+          
+7
+8
+
+9
+10
+
+11
+12
+
+
+Måndag
+
+16.15-18.45
+19.00-21.30
+
+Onsdag
+
+16.15-18.45
+19.00-21.30
+
+Fredag
+
+16.15-18.45
+19.00-21.30
+
+
+Kursanmälan - först till kvarn som gäller - fakturan skall betalas innan första
+kurstillfälle eller vid första kurstillfälle.
+
+Alltid 25% early bird rabatt på kursavgift vid anmälan minst 14 dagar innan kursstart. 
+Alltid 25% förmånsrabatt för studenter, pensionärer och arbetslösa.
+
+Hela kursavggiften betalas vid första kurstillfälle.
+
+Betalning med Bankgiro, Swish eller Kort. 14 dagars faktura för företagskunder.
+
+
+
+
+
+
+*/
 
